0f9101a25196dc25e756c52d1d308e28aa0a1857
Kakashixlol <142254758+Kakashixlol@users.noreply.github.com>
2023-10-22 00:29:21 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -12,7 +12,7 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['94784506970']
+global.owner = ['201033401721']
 global.premium = ['94784506970']
 global.ownernomer = '94784506970'
 global.ownername = 'Achi Fernando'
